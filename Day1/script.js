5c71757635706f234098398a9c242f077ce05a83
AHzizi <miftakulazizi0@gmail.com>
2024-03-15 20:20:35 +0700

    update day1

---
+++ Day1/script.js
@@ -0,0 +1,25 @@
+// This Is Comment
+
+//Welcome to 30DaysOfJavaScript
+
+/*This Is Multyple Comment */
+
+let str = `Hello World!`;
+let bool = false;
+let und = null;
+
+console.log(typeof und);
+
+let a;
+let b;
+let c;
+let d;
+
+a = 'AHzizi';
+b = 'Renri';
+c = 21;
+d = `Jungsemi`;
+
+const cetak = a + b + c + d;
+
+console.log(cetak);
